04a6eac006a124f1e97114513a9df7d89846a8aa
JeongWon Lim <cdnljw@gmail.com>
2020-10-26 23:46:30 +0900

    STUDY : mpl_finance, matplolib.ticker, matplolib.gridspec 활용
    문제
    1. 지난 시간 그래프 연동 성공
    2. df파일 데이터 정확했음
    3. 여러그래프 생성 해결
    
    해결
    1. add_subplot 함수 내에서 sharex, sharey 매개변수로 이전 그래프 변수를
    삽입하면 서로 x축 또는 y 값이 연동됨
    2. df 파일은 확인한바 정확했음
    3. 틀을 생성하는 변수는 초기화 함수에 넣었음, draw함수가 있어야 그렸던
    그래프는 qt내에서 출력가능
    
    다음문제
    - 데이터가 많아질 경우 그래프 출력하는 것이 너무 느림
    - 툴바를 이용시 y 축을 고정할 수 는 없는지
    - bar차트를 출력하는 방법

--- __init__.py
+++ __init__.py
@@ -6,6 +6,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+import matplotlib.gridspec as gridspec
+import matplotlib.ticker as ticker
+import mpl_finance
 import plotly
 import time
 
@@ -34,6 +37,18 @@ class Ui_MainEvent(UIM, QtWidgets.QMainWindow):
 
         self.stockCBB.addItems(self.kiwoom.jongmokCode)
 
+        self.fig = plt.Figure()
+        self.canvas = FigureCanvas(self.fig)
+        self.toolbar = NavigationToolbar(self.canvas, self)
+
+        self.verticalLayout.addWidget(self.canvas)
+        self.verticalLayout.addWidget(self.toolbar)
+
+        self.ax = self.fig.add_subplot(211)
+        self.bx = self.fig.add_subplot(212, sharex = self.ax)
+        # self.ax.label_outer()
+        # self.bx.label_outer()
+
     #정의
     def allEvent(self): #종목코드가 변경되었을 때 모든 데이터 및, Ui 재설정
         self.kiwoom.set_jongCode(self.stockCBB.currentText())
@@ -42,20 +57,20 @@ class Ui_MainEvent(UIM, QtWidgets.QMainWindow):
 
         print(data)
 
-        self.fig = plt.Figure()
-        self.canvas = FigureCanvas(self.fig)
-        self.toolbar =NavigationToolbar(self.canvas, self)
 
-        self.verticalLayout.addWidget(self.canvas)
-        self.verticalLayout.addWidget(self.toolbar)
-        print("1")
 
-        ax = self.fig.add_subplot(211)
         print('2')
-        ax.plot(data.index, data['고가'],label = 'a')
-        ax = self.fig.add_subplot(212)
-        ax.plot(data.index, data['거래량'],label ='b')
-        ax.grid()
+        self.ax.xaxis.set_major_formatter(ticker.FixedFormatter(data['일자'].tolist()))
+        num = []
+        for i in range(len(data['일자'].tolist())):
+            num.append(i)
+        self.ax.xaxis.set_major_locator(ticker.FixedLocator(num))
+
+        mpl_finance.candlestick2_ochl(self.ax, data['시가'], data['고가'], data['저가'], data['현재가'], width=0.5, colorup='r', colordown='b')
+
+        self.bx.plot(data.index, data['거래량'], label='b')
+        self.ax.grid()
+        self.bx.grid()
         print('3')
 
         self.canvas.draw()
